f98b582b1d8f4259b6dc73cabcbbec9b5d1957e0
Litchi Pi <litchi.pi@proton.me>
2023-10-31 12:38:43 +0100

    fixup grammar to make tests pass
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -11,31 +11,31 @@ use crate::Rule;
 #[test]
 fn test_parse_markdown_input() {
     let input = "
-    # Je suis un titre h1
-    ## Je suis un titre h2
-    [Je suis un texte de lien](je_suis_un_url)
+# Je suis un titre h1
+## Je suis un titre h2
+[Je suis un texte de lien](je_suis_un_url)
 
-    [Je suis un texte de référence de lien][je_suis_une_ref_de_lien]
+[Je suis un texte de reference de lien][je_suis_une_ref_de_lien]
 
-    > Je suis une quote
+> Je suis une quote
 
-    **Je suis bold**
+**Je suis bold**
 
-    *Je suis italique*
+*Je suis italique*
 
-    Je suis un bout de `code`
+Je suis un bout de `code`
 
-    ```
-        Je suis du code en block
-    ```
+```
+    Je suis du code en block
+```
 
-    ### h3
-    #### h4
-    ##### h5
-    ###### h6
+### h3
+#### h4
+##### h5
+###### h6
 
-    Bref
-    ";
+Bref
+";
 
     
     let parsed = MarkdownParser::parse(Rule::file, input);
